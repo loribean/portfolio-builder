67c25f311494e4481a6a3d503604acff44675823
“loribean” <aureliadotlim@gmail.com>
2020-10-21 17:17:20 +0800

    updated urls

--- djreact/urls.py
+++ djreact/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('index.html')),
+    
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
      path('dj-rest-auth/', include('dj_rest_auth.urls')),
