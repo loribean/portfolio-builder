781e19992ea22de5bae27cf227c8ea74d3d7b03c
“loribean” <aureliadotlim@gmail.com>
2020-10-21 20:31:29 +0800

    try try again

---
+++ build/precache-manifest.a40c96a35be202d15b59329ae1fda63b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46577fd5f8ab7ddef8e596be87a72b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3742258271026fde6782",
+    "url": "/static/css/2.ea2d4f28.chunk.css"
+  },
+  {
+    "revision": "9a32a90b616f3adb7f79",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3742258271026fde6782",
+    "url": "/static/js/2.7804d9fe.chunk.js"
+  },
+  {
+    "revision": "5bab3371eb171043d82ddfe95492922a",
+    "url": "/static/js/2.7804d9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a32a90b616f3adb7f79",
+    "url": "/static/js/main.6c6a680e.chunk.js"
+  },
+  {
+    "revision": "d96159fb79fad195664d",
+    "url": "/static/js/runtime-main.2dc790a7.js"
+  },
+  {
+    "revision": "38b30fa0229248d8bd3a14c802046743",
+    "url": "/static/media/main-fonts.38b30fa0.ttf"
+  },
+  {
+    "revision": "638868fe8f33b0b45c29fc4e4fba99a9",
+    "url": "/static/media/main-fonts.638868fe.woff"
+  },
+  {
+    "revision": "eceff26a5f33cc81123f437a16a78dbe",
+    "url": "/static/media/main-fonts.eceff26a.svg"
+  },
+  {
+    "revision": "f7c56e52c2c44690a0e39d5a486d0c3b",
+    "url": "/static/media/main-fonts.f7c56e52.eot"
+  }
+]);
