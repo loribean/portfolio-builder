76a4d87ae001313dd5accb6b7521a9fa23f7dfe6
“loribean” <aureliadotlim@gmail.com>
2020-10-21 15:00:24 +0800

    help

--- djreact/settings.py
+++ djreact/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['portfolio-builder-x.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
